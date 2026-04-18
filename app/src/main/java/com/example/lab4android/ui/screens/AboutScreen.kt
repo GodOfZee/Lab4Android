538c33635a15c8a7aaa4328e405250eaa5b32c75
GodOfZee <alex.mikr.06.03@gmail.com>
2026-04-19 01:53:20 +0400

    Добавлена возможность пролистывания на экраны настроек и и нформации о приложении

--- app/src/main/java/com/example/lab4android/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/example/lab4android/ui/screens/AboutScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,7 +26,8 @@ fun AboutScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(dimensionResource(R.dimen.padding_large)),
+            .padding(dimensionResource(R.dimen.padding_large))
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
--- app/src/main/java/com/example/lab4android/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/lab4android/ui/screens/SettingsScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
@@ -31,6 +33,7 @@ fun SettingsScreen(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize()
             .padding(dimensionResource(R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = stringResource(R.string.settings),
